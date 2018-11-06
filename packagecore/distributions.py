f13db601627bead4efff8a676cb7b8e46aadb334
Dominique LaSalle <dominique@solidlake.com>
2018-11-05 18:39:50 -0800

    Remove arch linux non-official container

--- packagecore/distributions.py
+++ packagecore/distributions.py
@@ -16,7 +16,9 @@ DATA = {
         "formatString": "{name}-{version}-{release}.al.2017.03.{arch}.rpm"
     },
     "archlinux": {
-        "dockerImage": "base/archlinux:latest",
+        # there is no official arch linux docker image, so users will have
+        # to specify one
+        "dockerImage": None,
         "packageType": "pkgbuild",
         "formatString": "{name}-{version}-{release}-{arch}.pkg.tar.xz"
     },
